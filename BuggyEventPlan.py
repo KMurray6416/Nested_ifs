74b5f9e2594cd1f64d0d66f1e4dc64dcdb87399a
KMurray6416 <kmurray6416@gmail.com>
2024-07-18 04:17:58 -0600

    Update BuggyEventPlan.py

--- BuggyEventPlan.py
+++ BuggyEventPlan.py
@@ -1,5 +1,11 @@
+    # Task 1
 attendees = int(input("Enter number of attendees: "))
-                
 venue = "large hall" if attendees > 100 else "conference room"
-
 print(venue)
+    # Task 2
+entertainment = "audio system" if attendees > 100 else "projector"
+print(entertainment)
+    # Task 3
+diet = input("What are your dietary needs? (vegetarian/non-vegetarian): ")
+catering = "Veggie Delight Caterers" if diet == "vegetarian" else "Gourmet Meals Caterers"
+print(catering)
